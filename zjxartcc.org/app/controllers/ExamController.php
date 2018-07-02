2f7e8dbe12ea4dc12f8b16ee08f887ef1c4f02ff
JKruse4 <jkruse4@me.com>
2018-07-01 21:44:39 -0400

    Update ExamController.php

--- zjxartcc.org/app/controllers/ExamController.php
+++ zjxartcc.org/app/controllers/ExamController.php
@@ -5,25 +5,25 @@ class ExamController extends BaseController {
 	public function showPendingTheoryExams()
 	{
 		$awaitingexams = Exam::where('controller_id', Auth::id())->where('complete', '0')->orderBy('updated_at', 'DESC')->get();
-		return View::make('admin.training.exams.pt')->with('awaitingexams', $awaitingexams);
+		return View::make('admin.training.exams.pt')->withErrors(['awaitingexams', $awaitingexams]);
 	}
 
 	public function showCompletedTheoryExams()
 	{
 		$completedexams = Exam::where('controller_id', Auth::id())->where('complete', '1')->orderBy('updated_at', 'DESC')->get();
-		return View::make('admin.training.exams.ct')->with('completedexams', $completedexams);
+		return View::make('admin.training.exams.ct')->withErrors(['completedexams', $completedexams]);
 	}
 
 	public function showPendingPracticalExams()
 	{
 		$ots = OTSRequest::where('controller_id', Auth::id())->where('complete', '0')->orderBy('updated_at', 'DESC')->get();
-		return View::make('admin.training.exams.pp')->with('ots', $ots);
+		return View::make('admin.training.exams.pp')->withErrors(['ots', $ots]);
 	}
 
 	public function showCompletedPracticalExams()
 	{
 		$ots = OTSRequest::where('controller_id', Auth::id())->where('complete', '1')->orderBy('updated_at', 'DESC')->get();
-		return View::make('admin.training.exams.cp')->with('ots', $ots);
+		return View::make('admin.training.exams.cp')->withErrors(['ots', $ots]);
 	}
 
 }
